b29162a931936fbdfe1f10ab447b74992ff8a7c9
LPeter-nm <pg.nasmatos@outlook.com>
2025-02-15 11:42:47 -0300

    feat: adicionando arquivo para criar adm geral manualmente

---
+++ src/create-general.ts
@@ -0,0 +1,46 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcryptjs';
+import { randomInt } from 'crypto';
+
+const prisma = new PrismaClient();
+
+async function createGeneral() {
+  try {
+    const checkGeneral = await prisma.user.findFirst({
+      where: { role: 'GENERAL' },
+    });
+
+    // Garantindo que haja apenas um admin geral no sistema
+    if (checkGeneral)
+      throw new HttpException(
+        'Administrador geral já registrado',
+        HttpStatus.CONFLICT,
+      );
+
+    const general = 'general_admin@example.com';
+    const password = 'passexample';
+    const name = 'Administrador geral';
+
+    const randomSalt = randomInt(10, 16);
+    const hashedPassword = await bcrypt.hash(password, randomSalt);
+
+    const generalAdmin = await prisma.user.create({
+      data: {
+        name,
+        email: general,
+        password: hashedPassword,
+        type_User: 'SERVIDOR',
+        role: 'GENERAL',
+      },
+    });
+
+    console.log('Administrador geral registrado no sistema', generalAdmin);
+  } catch (error) {
+    console.error('Erro ao registrar admnistrador geral manualmente', error);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+createGeneral();
