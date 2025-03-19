b9dfff8b51db506f68bc1c446feef6df08587cfa
LPeter-nm <pg.nasmatos@outlook.com>
2025-03-19 15:34:20 -0300

    feat: adicionando validação de datas para não haver datas passadas

---
+++ src/validations/reservationDateValidate.ts
@@ -0,0 +1,14 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
+
+export function validateReservationDates(dateStart: Date, dateEnd: Date) {
+  const currentDate = new Date();
+
+  currentDate.setHours(0, 0, 0, 0);
+
+  if (dateStart < currentDate || dateEnd < currentDate) {
+    throw new HttpException(
+      'Não é permitido registrar reservas em datas anteriores à data atual.',
+      HttpStatus.BAD_REQUEST,
+    );
+  }
+}
